998826698ecbe2f266e0b76b3d33ca0057d61228
Salma EL HAJRAOUI <selhajraoui@ORSID-PROD.dns.op.com>
2020-10-01 17:55:35 +0200

    'draw'

---
+++ app.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "white";
+ctx.strokeStyle = "blue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+let drawPieces = (piece) => {
+
+}
+
+let drawSnake = (snake) => {
+
+}
+
