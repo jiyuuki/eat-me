f948ac431537f447b0414bbefe2cbb5474dd9bc9
Salma EL HAJRAOUI <selhajraoui@ORSID-PROD.dns.op.com>
2020-10-05 17:36:22 +0200

    move snake

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const snake = [{ x: 150, y: 100 }, { x: 140, y: 100 }, { x: 130, y: 100 }, { x: 120, y: 100 }];
 
 ctx.fillStyle = "white";
-ctx.strokeStyle = "blue";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.strokeStyle = "blue";
 ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
 let drawPieces = (piece) => {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(piece.x, piece.y, 10, 10);
+
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(piece.x, piece.y, 10, 10);
 
 }
 
-let drawSnake = (snake) => {
-
+let drawSnake = () => {
+    snake.forEach(piece => {
+        drawPieces(piece);
+    });
 }
 
+let moveSnake = () => {
+    let head = { x: snake[0].x + 10, y: snake[0].y };
+    snake.unshift(head);
+    snake.pop();
+}
+
+moveSnake();
+drawSnake();
